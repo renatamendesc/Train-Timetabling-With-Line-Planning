29544c2bab0e4989436a2913cb56787cff142a4b
Renata Mendes <renatampc030802@gmail.com>
2025-09-26 16:18:01 -0300

    increasing time limit

--- src/Model.cpp
+++ src/Model.cpp
@@ -21,7 +21,7 @@ int Model::run (Data &data)
     // set time limit
     int time_limit = 43200;
     if (verify_feasibility)
-        time_limit = 3600;
+        time_limit = 43200;
 
     // create decision variables
     add_variables(data);
